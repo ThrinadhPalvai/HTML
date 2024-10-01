5a7c05e314d297789a3b9d34c5e3130322336456
ThrinadhPalvai <thrinadhpalvai1106@gmail.com>
2024-10-01 13:57:37 +0530

    firstcommit

---
+++ python proposal.py
@@ -0,0 +1,26 @@
+
+	import tutrtle
+	pen=turtle.Turtle()
+	def curve():
+	for i in range(200)
+	pen.right(1)
+	def herart()
+	pen.fillcolr('red')
+	pen.begin_fill()
+	pen.left(140)
+	pen.forward(113)
+	curve()
+	pen.left(120)
+	curve()
+	pen.forward(112)
+	pen.end_fill()
+	def txt():
+	pen.up()
+	pen.setpos(-68,95)
+	pen.down()
+	pen.color('lightgreen')
+	pen.write("love you",font=(
+	"verdana",30,"bold"))
+	heart()
+	txt()
+	pen.ht()
